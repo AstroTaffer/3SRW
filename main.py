9c5446cfcfea60991755dc51522e54d147ea4730
Daniel <89512722+AstroTaffer@users.noreply.github.com>
2021-09-13 18:57:17 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from data import SRWData
 
 
-image_path = "H:/CODING_LAIR/IMAGES/EAST_V/"
-daob = SRWData(image_path, aperture=6, image_filter="V")
+image_path = "H:/CODING_LAIR/IMAGES/WEST_R/"
+daob = SRWData(image_path, aperture=8, image_filter="R")
 
 # daob.create_catalog()
 # daob.write_catalog_file()
-daob.read_catalog_file()
+# daob.read_catalog_file()
 
-daob.aperture_photometry()
-daob.write_raw_files()
+# daob.aperture_photometry()
+# daob.write_raw_files()
 # daob.read_raw_files()
 
-daob.differential_photometry()
-daob.write_clear_files()
+# daob.differential_photometry()
+# daob.write_clear_files()
 # daob.read_clear_files()
 
-
+"""
 # ALL
 daob.pars["vss_method"] = "ALL"
 daob.vs_search()
 daob.write_vs_mask()
 daob.plot_vs_light_curves()
-
+"""
 
 """
 # ERRORS
